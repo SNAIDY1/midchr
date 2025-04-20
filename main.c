7460858f3cef4ea5675572bb597c211bd10c6e2d
SNAIDY1 <hjxl12341234@gmail.com>
2025-04-20 03:25:16 +0300

    write "How To Use midchr Program "

--- main.c
+++ main.c
@@ -1,8 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <own/termcolor.h>
+
+
+
+
+
+
+/* TODO:
+ * 1 - Spearate Project Into 2 Files
+ * 2 - Write These Functions:
+ * 	* Midchr(const char *msg,int color): Center Width And Hight
+ * 		must be hex or a symbol
+ * 	* MidchrR(const char *msg)
+ * 	* MidchrC(const char *msg)
+ * 	* 
+ * 	* 
+ *  
+ *
+ *
+ * */
+
+
+char* rgb;
+static int rgbColors[3];
+
+
+
+int* HexToInt(char *rgbHex);
+int* HexToInt(char *rgbHex) {
+
+
+	if (strncmp(rgbHex,"0x",2) == 0)
+	{
+		memmove(rgbHex, rgbHex + 2, strlen(rgbHex + 2) + 1);
+	}
+
+	char r[3] = {rgbHex[0],rgbHex[1],'\0'};
+	char g[3] = {rgbHex[2],rgbHex[3],'\0'};
+	char b[3] = {rgbHex[4],rgbHex[5],'\0'};
+
+
+	rgbColors[0] = (int)strtol(r,NULL,16);
+	rgbColors[1] = (int)strtol(g,NULL,16);
+	rgbColors[2] = (int)strtol(b,NULL,16);
+
+	return rgbColors;
+}
 
 char *CenterString(char *msg);
 char *CenterString(char *msg)
@@ -11,16 +58,16 @@ char *CenterString(char *msg)
 	ioctl(STDOUT_FILENO,TIOCGWINSZ,&w);
 	int row = w.ws_row;
 	int col = w.ws_col;
-	int centerXText = col/2+strlen(msg)/2;
+	int centerText = col/2+strlen(msg)/2;
+	printf("col:%d row:%d",col,row);
 
 
-	//printf("lines: %d\ncolumns: %d\n",w.ws_row,w.ws_col);
-	for (int i = 0;i < row/2+5;i++)
+	for (int i = 0;i < row/2;i++)
 	{
 		printf("\n");
 	
 	}
-	printf("%*s",centerXText,msg);
+	printf("%*s%s",centerText,msg,CRESET);
 	for (int i = 0;i < row/2;i++)
 	{
 		printf("\n");
@@ -33,27 +80,130 @@ char *CenterString(char *msg)
 int main(int argc,char *argv[]) {
 
 
-	if (!argv[1] 
-	|| strcmp(argv[1],"--help") == 0 
-	|| strcmp(argv[1],"help") == 0 
-	|| strcmp(argv[1],"-h") == 0 )
+
+	for (int i = 0;i < argc;i++)
 	{
-	
-		printf("midchr: Middle Characters\n");
+		if (!argv[1] 
+		|| strcmp(argv[1],"--help") == 0 
+		|| strcmp(argv[1],"help") == 0 
+		|| strcmp(argv[1],"-h") == 0 )
+		{
+		
+			printf("midchr: Middle Characters\n");
 
-		printf(BWHT "USE:\n" CRESET);
-		printf("\tmidchr [OPTION] \"Arg\" \n");
+			printf(BWHT "USE:\n" CRESET);
+			printf("\tmidchr \"Text\" [OPTION] \n");
+			
 
-		/*
-		printf(BWHT "\nOPTIONS:" CRESET);
-		printf("\n\t");
-		printf("-fg=COLOR\tForeground Color\n");
-		printf("\n\t");
-		printf("-bg=COLOR\tBackground Color\n");
-		*/
-		return -1;
+			printf(BWHT "\nOPTIONS:" CRESET);
+			
+			printf("\n\t");
+			printf("--rgb=HEX\tRGB Colors (Only HEX)");
+
+			printf("\n\t");
+			printf("--green\t\tGreen Foreground");
+			printf("\n\t");
+			printf("--red\t\tRed Foreground");
+			printf("\n\t");
+			printf("--blue\t\tBlue Foreground");
+			printf("\n\t");
+			printf("--magenta\tMagenta Foreground");
+			printf("\n\t");
+			printf("--black\t\tBlack Foreground");
+			printf("\n\t");
+			printf("--yellow\tYellow Foreground");
+			printf("\n\t");
+			printf("--cyan\t\tCyna Foreground");
+			printf("\n\t");
+			printf("--white\t\tWhite Foreground");
+			printf("\n");
+
+
+			printf("\n\t");
+			printf("-B\t\tBold Text");
+			printf("\n\t");
+			printf("-b\t\tBlink Text (doesn't work on some terminals)");
+			printf("\n\t");
+			printf("-i\t\tItalic Text ");
+			printf("\n");
+
+			return -1;
+		}
+		
+		else if (strcmp(argv[i],"-B") == 0)
+		{
+			printf(BLD " ");
+		}
+
+		else if (strcmp(argv[i],"-b") == 0)
+		{
+			printf(BLN " ");
+		}
+		else if (strcmp(argv[i],"-i") == 0)
+		{
+			printf(ITC " ");
+		}
+
+
+
+
+		
+		
+		else if (strncmp(argv[i],"--rgb=",6) == 0)
+		{
+			rgb = argv[i] + strlen(argv[i]) - 6 ;
+
+			int *rgbShColors = HexToInt(rgb);
+			
+
+
+			printf("\e[38;2;%d;%d;%dm",rgbShColors[0],
+					rgbShColors[1],rgbShColors[2]);
+			
+		}
+
+		else if (strcmp(argv[i],"--red") == 0)
+		{
+			printf(RED " ");
+		}
+
+		else if (strcmp(argv[i],"--blue") == 0)
+		{
+			printf(BLU " ");
+		}
+
+		else if (strcmp(argv[i],"--magenta") == 0)
+		{
+			printf(MAG " ");
+		}
+
+		else if (strcmp(argv[i],"--black") == 0)
+		{
+			printf(BLK " ");
+		}
+
+		else if (strcmp(argv[i],"--green") == 0)
+		{
+			printf(GRN " ");
+		}
+
+		else if (strcmp(argv[i],"--yellow") == 0)
+		{
+			printf(YEL " ");
+		}
+		else if (strcmp(argv[i],"--cyan") == 0)
+		{
+			printf(CYN " ");
+		}
+		else if (strcmp(argv[i],"--white") == 0)
+		{
+			printf(WHT " ");
+		}
+
 	}
 	CenterString(argv[1]);
+
+
 
 
 	return 0;
